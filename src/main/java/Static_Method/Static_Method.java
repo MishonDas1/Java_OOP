525a7cb4dc7ca551f635daaf1e3debd2eb0a8fbe
Mishon24 <mishon.da@uttarauniversity.edu.bd>
2025-04-27 12:54:05 +0600

    Static Method

---
+++ src/main/java/Static_Method/Static_Method.java
@@ -0,0 +1,14 @@
+
+package Static_Method;
+
+
+public class Static_Method {
+    void Display1(){
+        System.out.println("I am static Method");
+    }
+     static void Display2(){
+        System.out.println("I am non static Method");
+    }
+    
+    
+}
---
+++ src/main/java/Static_Method/Test.java
@@ -0,0 +1,12 @@
+
+package Static_Method;
+
+
+public class Test {
+    public static void main(String[] args) {
+        Static_Method s1 = new Static_Method();
+        s1.Display1();
+        
+        Static_Method.Display2();
+    }
+}
